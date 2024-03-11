6d733a6c878268df703b7fa397880d3b880c4fe2
BotezDaniel <162982600+BotezDaniel@users.noreply.github.com>
2024-03-11 12:27:07 +0000

    Update and rename 2. Second Step: Install dnsmasq and hostapd to 2. Second Step: Install dnsmasq and hostapd.py

--- 2. Second Step: Install dnsmasq and hostapd.py
+++ 2. Second Step: Install dnsmasq and hostapd.py
@@ -1,8 +1,8 @@
-To install both softwares, I issued this command:
+#To install both softwares, I issued this command:
 sudo apt-get install hostapd dnsmasq
 
 
-After the installation, this is how I set up dnsmasq:
+#After the installation, this is how I set up dnsmasq:
 
 interface=wlan0         
 listen-address=10.0.1.1 
@@ -12,5 +12,5 @@ bogus-priv
 dhcp-range=10.0.1.2,10.0.1.16,9h 
 
 
-And then I enabled dnsmasq service to start at boot using this command:
+#And then I enabled dnsmasq service to start at boot using this command:
 sudo systemctl enable dnsmasq
